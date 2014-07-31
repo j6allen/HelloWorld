115c50daaff2b7797b52c88359d68dad08f7940e
Jesse Allen <j6allen@gmail.com>
2014-07-31 16:18:40 -0700

    Application originally written on 8/20/2013

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+* a Simply application that displays "Hello World!" 
+*
+* @author  Jesse Allen
+* @version 1.0
+* @since   2013-08-20 
+*/
+public class HelloWorld {
+	public static void main(String[] args)
+		{
+			//Displays "Hello World" 
+			System.out.print("Hello World");
+		}			
+	}
+
